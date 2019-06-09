030b964aedd004118a2dafb8b903bb8a16142c36
stephanfalcon <hoobaloob99@gmail.com>
2019-06-09 10:55:48 -0600

    routes are linked to server

---
+++ routes/routes.js
@@ -0,0 +1,9 @@
+const express = require("express")
+const router = express.Router()
+const path = require("path")
+
+router.get("/",function(req,res){
+    res.send("<p>hello</p>")
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 //express
 const express = require("express");
-
+const router = require("./routes/routes")
 const port = 3500;
 
 const app = express();
 
+app.use(router)
+
 app.listen(port,function(){
     console.log(`listening on port ${port}`);
 })
-
-module.exports = app
