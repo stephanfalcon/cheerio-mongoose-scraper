cd2a2407aca5c5b0a51275c74ec5aac6f2732a1b
stephanfalcon <hoobaloob99@gmail.com>
2019-07-07 12:44:06 -0600

    fdsaf

--- controller/dbcoms.js
+++ controller/dbcoms.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 const db = require("../models/articles.js") // need to be changed and index needs to be added to models
 const scraper = require("../scraper/scraper.js")
-mongoose.connect("mongodb://localhost/news", { useNewUrlParser: true });
+mongoose.connect("mongodb://heroku_gpqztw2q:l1c3adaa1grfs54bpnio4tf7ln@ds347917.mlab.com:47917/heroku_gpqztw2q", { useNewUrlParser: true });
 
 const orm = {
     find: function(col,cb){
